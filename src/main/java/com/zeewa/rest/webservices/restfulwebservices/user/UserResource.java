887d15929a45ce6eecf05bc852263f2ed192f14f
Mohamed Aweez Akram <aweezakram1999@gmail.com>
2023-10-10 02:58:28 +0530

    Implement POST method to create User Resource

--- src/main/java/com/zeewa/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/zeewa/rest/webservices/restfulwebservices/user/UserResource.java
@@ -5,22 +5,29 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserResource {
-	
+
 	@Autowired
 	UserDaoService userDaoService;
-	
+
 	@GetMapping(path = "/users")
 	public List<User> retrieveAllUsers() {
 		return userDaoService.findAll();
 	}
-	
+
 	@GetMapping(path = "/users/{id}")
 	public User retrieveUser(@PathVariable int id) {
 		return userDaoService.findOne(id);
+	}
+
+	@PostMapping(path = "/users")
+	public void createUser(@RequestBody User user) {
+		userDaoService.save(user);
 	}
 
 }

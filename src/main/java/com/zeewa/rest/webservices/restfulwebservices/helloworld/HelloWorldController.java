6cf1b45da2c19d7a0ea5b39358246948cf8f1e77
Mohamed Aweez Akram <aweezakram1999@gmail.com>
2023-10-10 02:26:25 +0530

    Enhance Hello World API with a path variable

--- src/main/java/com/zeewa/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/zeewa/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.zeewa.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,10 +11,15 @@ public class HelloWorldController {
 	public String helloWorld() {
 		return "Hello World";
 	}
-	
+
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World");
+	}
+
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldBean(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
 
 }

9da5e59fc7431f544f259da209f0347200249b2b
Mohamed Aweez Akram <aweezakram1999@gmail.com>
2023-10-10 02:03:57 +0530

    Enhance Hello World API to return a bean

---
+++ src/main/java/com/zeewa/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
@@ -0,0 +1,28 @@
+package com.zeewa.rest.webservices.restfulwebservices.helloworld;
+
+public class HelloWorldBean {
+
+	private String message;
+
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("HelloWorldBean [message=");
+		builder.append(message);
+		builder.append("]");
+		return builder.toString();
+	}
+
+}

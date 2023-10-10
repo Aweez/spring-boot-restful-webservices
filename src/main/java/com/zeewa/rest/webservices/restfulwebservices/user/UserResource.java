ea357d99beab8ba6f80a51687948e131b0490d68
Mohamed Aweez Akram <aweezakram1999@gmail.com>
2023-10-11 01:37:13 +0530

    Enhance POST method to return correct HTTP status code and Location as part of the response header

--- src/main/java/com/zeewa/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/zeewa/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,13 +1,16 @@
 package com.zeewa.rest.webservices.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -26,8 +29,15 @@ public class UserResource {
 	}
 
 	@PostMapping(path = "/users")
-	public void createUser(@RequestBody User user) {
-		userDaoService.save(user);
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		User savedUser = userDaoService.save(user);
+		URI location = ServletUriComponentsBuilder
+				.fromCurrentRequest() // Gets the current request path - "/users"
+				.path("/{id}") // Appends "/{id}" to current request path - "users/{id}"
+				.buildAndExpand(savedUser.getId()) // Replaces {id} with the value
+				.toUri();
+		// location will be the location header in the response
+		return ResponseEntity.created(location).build();
 	}
 
 }
